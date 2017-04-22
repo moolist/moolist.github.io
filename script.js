191668c8ca38479b8c74fe3bc88d2c3ab216c88c
mooehkalu <mooehkalu@gmail.com>
2017-04-22 00:02:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -304,12 +304,12 @@ TheList += TheRow;
 if (arrayLength > 0)
 {
   document.getElementById("MyList").innerHTML = '<ul>' + TheList + '</ul>';
-document.getElementById("sharebutton").innerHTML = btnsharelist;
+
 
 }else
 {
   document.getElementById("MyList").innerHTML = '';
-document.getElementById("sharebutton").innerHTML = '';
+
 
 }
 }
