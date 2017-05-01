7989aafdee3103e159903d0783e48ae5e19496a3
mooehkalu <mooehkalu@gmail.com>
2017-05-01 11:02:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -263,7 +263,7 @@ for (var i = 0; i < arrayLength; i++) {
 var btndelete =  ' <input name="delete" style="visibility:hidden" type="" value="" onclick="deleteShoppinglists(' + i + ')" />';
 var arrays = shoppinglist[i];
 arrays = "'"+arrays+"'";
-var btnaddcart =  '<label><input name="add" type="checkbox" id="adds" value="Add to Shopping Cart" onclick="addtoshopcart('+arrays+','+ i +')" style="visibility:hidden"  /><img  id="adds" src="https://cdn.pbrd.co/images/WG1QoCXf.png" alt=""/><br></label>';
+var btnaddcart =  '<label><input name="add" type="checkbox" id="adds" value="Add to Shopping Cart" onclick="addtoshopcart('+arrays+','+ i +')" style="visibility:hidden"  /><img  id="adds" src="https://cdn.pbrd.co/images/WG1QoCXf.png" alt=""/><br><hr></label>';
 var btnsharelist = '<input class="button" id="shares" name="shares" type="submit" value="Share" onclick="share()" />';
 
  TheRow = '<li>' + shoppinglist[i] + btndelete + ' '  + btnaddcart + '</li>';
@@ -292,7 +292,7 @@ var btndelete =  ' <input name="delete" style="visibility:hidden" type="" value=
 var btnupdate =  ' <input class="button" name="edit" type="button" value="Edit Item" onclick="changeShoppingCart(' + i + ')" />';
 var arrays = addtocart[i];
 arrays = "'"+arrays+"'";
-var btnaddlist =  '<label><input name="add" type="checkbox" id="adds" value="Add to Shopping List" onclick="deleteShoppingCart(' + i + ')"  checked="checked" style="visibility:hidden"/><img  id="test" src="https://cdn.pbrd.co/images/61dedbPM3.png" alt=""/></label>';
+var btnaddlist =  '<label><input name="add" type="checkbox" id="adds" value="Add to Shopping List" onclick="deleteShoppingCart(' + i + ')"  checked="checked" style="visibility:hidden"/><img  id="test" src="https://cdn.pbrd.co/images/61dedbPM3.png" alt=""/><br></label>';
 var btnsharelist = '<input class="button" id="shares" name="shares" type="submit" value="Share " onclick="share()" />';
     TheRow =  "<li>" + addtocart[i] + btndelete + ' ' +  ' ' + btnaddlist + '<br></li>';
 TheList += TheRow;
